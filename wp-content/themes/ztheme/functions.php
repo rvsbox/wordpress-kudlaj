21c09ed4f871c6df235fdec0c0929c01f6a91fbf
rvsbox <rvsbox@live.ru>
2019-05-16 20:19:27 +0200

    l7. Connecting styles, scripts and installing dependencies in functions.php

--- wp-content/themes/ztheme/functions.php
+++ wp-content/themes/ztheme/functions.php
@@ -1,14 +1,19 @@
 <?php
 
-// подключение скриптов и стилей.
-// В этом коммите подключения пока не работают
+// подключение скриптов и стилей
 // ======================================================================
 
 function ztheme_scripts() {
     wp_enqueue_style('ztheme-bootstrapcss', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css');
     wp_enqueue_style('ztheme-style', get_stylesheet_uri());
 
-    wp_enqueue_script('ztheme-popper', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'); // можно без https
-    wp_enqueue_script('ztheme-bootstrapjs', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js');
+    wp_deregister_script( 'jquery' ); // WP имеет в комплекте jquery, соответственно необходимо удалить базовую регистрацию jquery
+    wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), false, true); // подключаем свой скрипт
+
+    wp_enqueue_script('ztheme-popper', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array('jquery'), false, true); // jquery - установка зависимости
+    wp_enqueue_script('ztheme-bootstrapjs', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js', array('jquery'), false, true);
 }
+
+add_action('wp_enqueue_scripts', 'ztheme_scripts');
+
 
