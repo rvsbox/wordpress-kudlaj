ad8cfc6eb2834381d9d5a54d1eb22764d77bde86
rvsbox <rvsbox@live.ru>
2019-05-17 23:10:33 +0200

    l14. Refactoring pagination

--- wp-content/themes/ztheme/functions.php
+++ wp-content/themes/ztheme/functions.php
@@ -16,6 +16,7 @@ function ztheme_scripts() {
 
 add_action('wp_enqueue_scripts', 'ztheme_scripts');
 
+
 // функция add_theme_support() для возможности добавления миниатюр в посты
 // ======================================================================
 
@@ -25,3 +26,29 @@ function ztheme_setup() {
 }
 
 add_action('after_setup_theme', 'ztheme_setup');
+
+
+// удаляет H2 из шаблона пагинации
+// ======================================================================
+
+add_filter('navigation_markup_template', 'my_navigation_template', 10, 2);
+function my_navigation_template($template, $class) {
+    /*
+    Вид базового шаблона:
+    <nav class="navigation %1$s" role="navigation">
+        <h2 class="screen-reader-text">%2$s</h2>
+        <div class="nav-links">%3$s</div>
+    </nav>
+    */
+
+    return '
+    <nav class="navigation" role="navigation">
+        <div class="nav-links">%3$s</div>
+    </nav>    
+    ';
+}
+
+// выводим пагинацию
+the_posts_pagination(array(
+    'end_size' => 2,
+));
--- wp-content/themes/ztheme/index.php
+++ wp-content/themes/ztheme/index.php
@@ -2,39 +2,52 @@
 
 <div class="container">
     <div class="row">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <!-- post -->
+        <?php if (have_posts()) :
+            // пагинация вверху страници для примера
+            the_posts_pagination(array(
+                'show_all' => false,
+                'end_size' => 1,
+                'mid_size' => 2,
+                'type' => 'list'
+            ));
+            while (have_posts()) : the_post(); ?>
+                <!-- post -->
 
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    </div>
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        </div>
 
-                    <div class="card-body">
-                        <?php if (has_post_thumbnail()): ?>
-                            <?php the_post_thumbnail('thumbnail'); ?>
-                        <?php else: ?>
-                            <img src="https://picsum.photos/150/150?random=1" width="150" height="150" alt=""
-                                 class="float-left mr-3">
-                        <?php endif; ?>
-                        <!-- см. get_the_title() -->
-                        <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <!-- см. get_the_excerpt(). Используется для цитаты -->
-                        <p class="card-text"><?php the_excerpt(); //the_content('Continue...');?></p>
+                        <div class="card-body">
+                            <?php if (has_post_thumbnail()): ?>
+                                <?php the_post_thumbnail('thumbnail'); ?>
+                            <?php else: ?>
+                                <img src="https://picsum.photos/150/150?random=1" width="150" height="150" alt=""
+                                     class="float-left mr-3">
+                            <?php endif; ?>
+                            <!-- см. get_the_title() -->
+                            <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                            <!-- см. get_the_excerpt(). Используется для цитаты -->
+                            <p class="card-text"><?php the_excerpt(); //the_content('Continue...');?></p>
 
-                    </div>
+                        </div>
 
-                    <div class="card-footer">
-                        <!-- см. get_permalink() -->
-                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go somewhere</a>
+                        <div class="card-footer">
+                            <!-- см. get_permalink() -->
+                            <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go somewhere</a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        <?php endwhile; ?>
+            <?php endwhile; ?>
             <!-- post navigation -->
-            <?php the_posts_pagination(); ?>
+            <?php the_posts_pagination(array(
+            'show_all' => false,
+            'end_size' => 1,
+            'mid_size' => 2,
+            'type' => 'list'
+        )); ?>
         <?php else: ?>
             <!-- no posts found -->
             <p>Постов нет...</p>
