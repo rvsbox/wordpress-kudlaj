3d33a6488afb26f9e35e95aa4cc31d008833cf54
rvsbox <rvsbox@live.ru>
2019-05-23 20:13:43 +0200

    b2. Removing tab in underscores

--- wp-content/themes/clean/template-parts/content-search.php
+++ wp-content/themes/clean/template-parts/content-search.php
@@ -10,26 +10,26 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    <header class="entry-header">
+        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			clean_posted_on();
-			clean_posted_by();
-			?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+        <?php if ( 'post' === get_post_type() ) : ?>
+        <div class="entry-meta">
+            <?php
+            clean_posted_on();
+            clean_posted_by();
+            ?>
+        </div><!-- .entry-meta -->
+        <?php endif; ?>
+    </header><!-- .entry-header -->
 
-	<?php clean_post_thumbnail(); ?>
+    <?php clean_post_thumbnail(); ?>
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php clean_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <footer class="entry-footer">
+        <?php clean_entry_footer(); ?>
+    </footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
