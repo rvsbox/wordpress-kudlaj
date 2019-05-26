a72dddd421e0d791a60b1d2b2c6d7a6fba2e5cdc
rvsbox <rvsbox@live.ru>
2019-05-26 15:33:04 +0200

    b9. Removing tab in /framework-customizations/extensions/icon

--- wp-content/themes/clean/framework-customizations/extensions/shortcodes/shortcodes/icon/config.php
+++ wp-content/themes/clean/framework-customizations/extensions/shortcodes/shortcodes/icon/config.php
@@ -3,7 +3,7 @@
 $cfg = array();
 
 $cfg['page_builder'] = array(
-	'title'         => __('Icon', 'fw'),
-	'description'   => __('Add an Icon', 'fw'),
-	'tab'           => __('Content Elements', 'fw'),
+    'title'         => __('Icon', 'fw'),
+    'description'   => __('Add an Icon', 'fw'),
+    'tab'           => __('Content Elements', 'fw'),
 );
--- wp-content/themes/clean/framework-customizations/extensions/shortcodes/shortcodes/icon/options.php
+++ wp-content/themes/clean/framework-customizations/extensions/shortcodes/shortcodes/icon/options.php
@@ -1,15 +1,15 @@
 <?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
+    die( 'Forbidden' );
 }
 
 $options = array(
-	'icon'       => array(
-		'type' => 'icon',
-		'label' => __( 'Icon', 'fw' )
-	),
-	'title'    => array(
-		'type'  => 'text',
-		'label' => __( 'Title', 'fw' ),
-		'desc'  => __( 'Icon title', 'fw' ),
-	)
+    'icon'       => array(
+        'type' => 'icon',
+        'label' => __( 'Icon', 'fw' )
+    ),
+    'title'    => array(
+        'type'  => 'text',
+        'label' => __( 'Title', 'fw' ),
+        'desc'  => __( 'Icon title', 'fw' ),
+    )
 );
