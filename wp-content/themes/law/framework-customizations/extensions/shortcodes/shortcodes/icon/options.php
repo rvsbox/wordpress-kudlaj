e62310db4958e6d1169ef3cd24e5cc7bd9d178a0
rvsbox <rvsbox@live.ru>
2019-07-10 22:49:20 +0200

    c6. Adding slider. Backup db

---
+++ wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/icon/options.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$options = array(
+	'icon'       => array(
+		'type' => 'icon',
+		'label' => __( 'Icon', 'fw' )
+	),
+	'title'    => array(
+		'type'  => 'text',
+		'label' => __( 'Title', 'fw' ),
+		'desc'  => __( 'Icon title', 'fw' ),
+	),
+
+    // custom
+    'url' => array(
+        'label' => __('Url', 'clean'),
+        'type'  => 'text',
+    ),
+);
