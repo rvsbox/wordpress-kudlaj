c3e2d6705eb256ad6ba064d8f97412157ec6a297
rvsbox <rvsbox@live.ru>
2019-07-15 19:58:22 +0200

    c10. Adding video.

--- wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/span/options.php
+++ wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/span/options.php
@@ -1,29 +1,38 @@
-<?php if ( ! defined( 'FW' ) ) {
-    die( 'Forbidden' );
+<?php if (!defined('FW')) {
+    die('Forbidden');
 }
 
 $options = array(
-    'span_icon'       => array(
-        'type' => 'icon',
-        'label' => __( 'Span Icon', 'fw' )
+    'span_wrapper' => array(
+        'type' => 'checkbox',
+        'label' => __('Wrap in div?', 'fw'),
+        'text' => __('', 'fw'),
     ),
-    'span_content'    => array(
-        'type'  => 'text',
-        'label' => __( 'Span Content', 'fw' ),
-        'desc'  => __( 'Span Content', 'fw' ),
+    'span_wrapper_class' => array(
+        'type' => 'text',
+        'label' => __('Wrapper Class', 'fw'),
+    ),
+    'span_icon' => array(
+        'type' => 'icon',
+        'label' => __('Span Icon', 'fw'),
+    ),
+    'span_content' => array(
+        'type' => 'text',
+        'label' => __('Span Content', 'fw'),
+        'desc' => __('Span Content', 'fw'),
     ),
     'span_class' => array(
-        'type'  => 'text',
+        'type' => 'text',
         'label' => __('Span Class', 'fw'),
         'desc' => __('Span Class', 'fw'),
     ),
     'span_id' => array(
-        'type'  => 'text',
+        'type' => 'text',
         'label' => __('Span ID', 'fw'),
         'desc' => __('Span ID', 'fw'),
     ),
     'span_data_attrs' => array(
-        'type'  => 'text',
+        'type' => 'text',
         'label' => __('Attributes', 'fw'),
         'desc' => __('Attributes', 'fw'),
     ),
