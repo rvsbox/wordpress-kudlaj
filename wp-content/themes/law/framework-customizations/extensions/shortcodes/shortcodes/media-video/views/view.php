8ff08a4162fb069f464e63cf32b5f485a6564828
rvsbox <rvsbox@live.ru>
2019-07-15 19:31:39 +0200

    c9. Adding video. Backup db

---
+++ wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/media-video/views/view.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined( 'FW' ) ) {
+    die( 'Forbidden' );
+}
+
+/**
+ * @var array $atts
+ */
+
+?>
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="<?php echo $atts['url'] ?>" allowfullscreen></iframe>
+</div>
