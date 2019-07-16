ddaeb0ee0876563b7225e17e952d3de105d5eaf4
rvsbox <rvsbox@live.ru>
2019-07-16 20:21:15 +0200

    c16. Adding sections of recent posts. Backup db

---
+++ wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/recent-posts/options.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
+
+$options = array(
+    'posts_number'       => array(
+        'type' => 'text',
+        'label' => __( 'Posts Number', 'fw' ),
+    ),
+);
