61058a6c809eb707d1d8918d8428aa9f1c62a421
rvsbox <rvsbox@live.ru>
2019-07-17 23:23:38 +0200

    c20. Adding templates index and single. Backup db

--- wp-content/themes/law/single.php
+++ wp-content/themes/law/single.php
@@ -1,37 +1,28 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package law
- */
+<?php get_header() ?>
 
-get_header();
-?>
+    <div id="fh5co-blog" class="fh5co-bg-section" style="background-color:#fcfcfc;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <?php
+                    if( has_post_thumbnail() ){
+                        $img_url = get_the_post_thumbnail_url();
+                    }else{
+                        $img_url = 'https://picsum.photos/800/570';
+                    }
+                    ?>
+                    <h1><?php echo the_title() ?></h1>
+                    <img class="pull-left law-post-image" src="<?php echo $img_url ?>" alt="">
+                    <?php the_content() ?>
+                <?php endwhile; ?>
+                    <!-- post navigation -->
+                <?php else: ?>
+                    <!-- no posts found -->
+                <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer() ?>
